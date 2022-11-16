8d47c7061636f63c349b96ef354e89b9b8604ce5
Akash Singh <50767686+manduakash@users.noreply.github.com>
2022-11-16 11:16:46 +0530

    Delete StudentManagementSystem directory

--- StudentManagementSystem/src/main/java/com/springboot/sms/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.sms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.sms.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-
-}
